c095b12776cc504649228371137879c8b559c809
ilias <ishaikh@ruffer.co.uk>
2018-11-07 16:38:37 +0000

    initial commit

---
+++ world/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace world.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IHostingEnvironment _env;
+
+        public HomeController(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_env);
+        }
+
+        public IActionResult View(string country)
+        {
+            throw new NotImplementedException();
+        }
+
+        // india/map
+        public IActionResult Map(string country)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
