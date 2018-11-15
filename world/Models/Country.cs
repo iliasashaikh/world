d6f5b2eec919c39200c3e6d8482c22788b680528
ilias <ishaikh@ruffer.co.uk>
2018-11-15 18:22:51 +0000

    updating the country model

--- world/Models/Country.cs
+++ world/Models/Country.cs
@@ -12,6 +12,9 @@ namespace world.Models
         public string Name { get; set; }
         public string Capital { get; set; }
         public string Flag { get; set; }
+        public long Population { get; set; }
+        public string Language { get; set; }
+        public string Currency { get; set; }
         public static List<string> CountryNames { get; set; }
     }
 }
